9d617d0f9a4cb58032310e21c53c27e311691759
Khamin Gleb <nimik19@yandex.ru>
2023-11-14 18:31:27 +0300

    8 Спринт, пробовал реализовать затенение (считаю что мой способ костыльный) и из расширенной версии реализовал логотип выше центра (тоже костыльно)

---
+++ ImageFeed/IImagesList/ImagesListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Глеб Хамин on 06.11.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+}
