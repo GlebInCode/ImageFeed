e197bb9b39547441567da0d37031e33eb8e13d1c
Khamin Gleb <nimik19@yandex.ru>
2023-12-09 13:56:43 +0300

    Созданы переменные с секретными ключами

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Глеб Хамин on 09.12.2023.
+//
+
+import Foundation
+
+let AccessKey = "JQJgJax-Jbr97jBjYuQLDZgaP5DX-b5Ckx1JA1pe09E"
+let SecretKey = "YL93TX8vKWwFQ6iZ9baPSxOEW_BGS_A8TUhJHLUlGVo"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = AccessKey + SecretKey + RedirectURI
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
