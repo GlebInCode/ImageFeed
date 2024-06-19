ec77c3e776c01c5530578a04a5d1679dbdfe2168
Khamin Gleb <nimik19@yandex.ru>
2024-06-19 12:24:44 +0300

    Таб бар реализован кодом. Настроен UIWindow.

---
+++ ImageFeed/TabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+//  Created by Глеб Хамин on 19.06.2024.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "profileOn"),
+            selectedImage: nil
+        )
+        
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+}
+
